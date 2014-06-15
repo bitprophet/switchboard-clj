0d13b3f0d446b2d510f297e01f1573fe9f88799f
Jeff Forcier <jeff@bitprophet.org>
2014-06-14 22:01:00 -0700

    $ lein new app switchboard

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to switchboard
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/switchboard/core_test.clj
@@ -0,0 +1,7 @@
+(ns switchboard.core-test
+  (:require [clojure.test :refer :all]
+            [switchboard.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
