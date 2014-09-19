13cfcb0e6dff6399c702874d7e855df34d71bdab
Jeff Forcier <jeff@bitprophet.org>
2014-09-19 11:36:22 -0700

    Some changes I barely remember making. But things work in a debuggy sense now?

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [ring/ring-core "1.3.0"]
                  [ring/ring-jetty-adapter "1.3.0"]]
   :plugins [[lein-ring "0.8.7"]]
-  :ring {:handler switchboard.core/handler :port 8000}
+  :ring {:handler switchboard.core/app :port 8000}
   :main ^:skip-aot switchboard.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/switchboard/core.clj
+++ src/switchboard/core.clj
@@ -1,21 +1,23 @@
 (ns switchboard.core
-  (:require [ring.adapter.jetty :refer :all]
-            [ring.util.request :refer :all])
+  (:require [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.util.response :refer [not-found, redirect]]
+            [clojure.string :refer [split]]
+            [clojure.pprint :refer [pprint]])
   (:gen-class))
 
-; TODO: nuke in prod or make only enabled when testing
-(require ['clojure.pprint :as 'pprint] :verbose)
-(defn pp [val] (with-out-str (pprint/pprint val)))
-; END TODO
+(defn dispatch [query]
+  (let [[key rest] (split query #" " 2)]
+    (pprint key)
+    (pprint rest)
+    {:body "hi"}))
 
 (defn handler [request]
-  {:status 200
-   :body (str
-           (pp request)
-           "\n-------\n\n"
-           (body-string request))
-   :headers {"Content-Type" "text/html"}})
+  (println) (println) (pprint request)
+  (if (contains? (:params request) :query)
+    (dispatch (:query (:params request)))
+    (not-found "What are you even doing?")))
 
-
-(defn -main [& args]
-  (run-jetty handler {:port 8000}))
+(def app (-> handler
+           wrap-keyword-params
+           wrap-params))
