b965b2ad730b15685c87617b6012edf3ab9144d0
Jeff Forcier <jeff@bitprophet.org>
2014-09-19 15:05:28 -0700

    Reorg/comment project file, like a newb

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject switchboard "0.1.0-SNAPSHOT"
+  ;; Core info
   :description "Browser search backend featuring shortcuts & smart searching."
   :url "http://example.com/FIXME"
   :license {:name "BSD 2-Clause License"
@@ -6,8 +7,14 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring/ring-core "1.3.0"]
                  [ring/ring-jetty-adapter "1.3.0"]]
-  :plugins [[lein-ring "0.8.7"]]
-  :ring {:handler switchboard.core/app :port 8000}
+
+  ;; Build-time options
   :main ^:skip-aot switchboard.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+
+  ;; Profiles
+  :profiles {:uberjar {:aot :all}}
+
+  ;; Dev-mode webserver execution
+  :plugins [[lein-ring "0.8.7"]]
+  :ring {:handler switchboard.core/app :port 8000})
