cfd25a01f2e267e8f91162c192365e2a890d4654
Jeff Forcier <jeff@bitprophet.org>
2015-07-19 16:12:05 -0700

    More refactor

--- test/switchboard/t_core.clj
+++ test/switchboard/t_core.clj
@@ -7,24 +7,25 @@
 (defn query [x] (core/app (mock/request :get "" {:query x})))
 (defn redirect [x] {:body "" :status 302 :headers {"Location" x}})
 
-(defn goog [x] (str "https://google.com/search?q=" x))
+(defn goog [x] (redirect (str "https://google.com/search?q=" x)))
+(defn gh [x] (redirect (str "https://github.com" x)))
 
 
-(facts "about general behavior"
+(facts "general behavior"
 
        (future-fact "lack of query param displays help info")
 
        (future-fact "present but empty query param value displays help info")
 
        (fact "if no submodule is matched, default is to Google"
-             (query "nope") => (redirect (goog "nope"))
-             (query "nope nohow") => (redirect (goog "nope nohow"))))
+             (query "nope") => (goog "nope")
+             (query "nope nohow") => (goog "nope nohow")))
 
 
 (facts "concerning github"
 
        (fact "bare key just hits homepage"
-             (query "gh") => (redirect "https://github.com"))
+             (query "gh") => (gh ""))
 
        (fact "bare project id just hits its landing page"
-             (query "gh inv") => (redirect "https://github.com/pyinvoke/invoke")))
+             (query "gh inv") => (gh "/pyinvoke/invoke")))
